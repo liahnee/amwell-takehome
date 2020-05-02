8e0b5fc37b0d97d5b75de4735d11636ee1558c88
liahnee <liahnee@gmail.com>
2020-05-01 19:48:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import logo from './logo.png';
+import './stylesheets/App.css';
 
 function App() {
+
+  const [ data, setData ] = useState('');
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const sortData = (data) => {
+    let int = data.map(item => parseInt(item))
+    int = int.filter((item, idx, arr) => {return arr.indexOf(item) == idx});
+    int.sort((a,b)=> a-b);
+    return int;
+  }
+
+  const fetchData = async () => {
+    await fetch('https://www.iwillfearnoevil.com/screen/string.txt')
+    .then(resp => resp.text())
+    .then(text => {
+      let data = text.split("\n")
+      setData(sortData(data));
+    })
+    .catch(error => {
+      console.log('Error', error)
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img id='logo' src={logo} alt="amwell logo"/>
+        <h1>Lowest Three Numbers</h1>
       </header>
+      <main className="App-main">
+        <p>
+          [ {data[0]}, {data[1]}, {data[2]} ]
+        </p>
+      </main>
     </div>
   );
 }
